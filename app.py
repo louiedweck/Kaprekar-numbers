51d1a99eb2e6e4e8144356d5744cd0cabcac02ce
louiedweck <dweck.louis@gmail.com>
2019-11-10 15:35:31 -0800

    added comments

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 def int_split_sum_combos(number: int) -> [int]:
+    ''' Takes in a number and finds all possible addition combination for the numbers inside.'''
     sums = []
     number_str = str(number)
     for i in range(len(str(number))-1):
@@ -9,10 +10,12 @@ def int_split_sum_combos(number: int) -> [int]:
 
 
 def is_kaprekar(n: int) -> bool:
+    ''' Checks if any combination of integers in number^2 = original number in function b4'''
     return n in int_split_sum_combos(n*n)
 
 
 def kaprekars_before(n: int) -> [int]:
+    ''' Returns all Kaprekar Numbers up to number run in function'''
     kaprekars = []
     for i in range(1, n+1):
         if is_kaprekar(i) and i % 10 != 0:
