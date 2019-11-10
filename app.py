f50f165154eafc43aa903ce2d4cd8bd407406b83
louiedweck <dweck.louis@gmail.com>
2019-11-10 00:44:14 -0500

    shows all kaprekar numbers up to input number

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ def int_split_sum_combos(number: int) -> [int]:
         suffix = int(number_str[i+1:])
         sums.append(prefix + suffix)
     return sums
+
+
+def is_kaprekar(n: int) -> bool:
+    return n in int_split_sum_combos(n*n)
+
+
+def kaprekars_before(n: int) -> [int]:
+    kaprekars = []
+    for i in range(1, n+1):
+        if is_kaprekar(i) and i % 10 != 0:
+            kaprekars.append(i)
+    return kaprekars
